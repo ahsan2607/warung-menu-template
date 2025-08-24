805cf87b7739607c6ca36b086cd61cea732a6bc1
Ahsan <firstsulthangalaxias@gmail.com>
2025-08-24 18:46:55 +0700

    Add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 // app/menu/page.tsx (server component by default)
 import { OrderProvider } from "@/context/OrderContext";
 import MenuTemplate from "@/components/menu/MenuTemplate";
+import { Suspense } from "react";
 
 export default function MenuPage() {
   return (
     <OrderProvider>
-      <MenuTemplate />
+      <Suspense fallback={<div>Loading...</div>}>
+        <MenuTemplate />
+      </Suspense>
     </OrderProvider>
   );
 }
